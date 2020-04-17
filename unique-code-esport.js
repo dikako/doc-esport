14ded9e2c9db74887740fa5e52b771e44e6acb10
Dika Koko <49613928+dikako@users.noreply.github.com>
2020-04-17 10:28:25 +0700

    unique-code-esport.js
    unique-code-esport.js

---
+++ unique-code-esport.js
@@ -0,0 +1,15 @@
+var request = require('request');
+var options = {
+  'method': 'POST',
+  'url': 'https://rc-api.rctiplus.com/esport/api/v1/unique_code',
+  'headers': {
+    'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2aWQiOjUxMDcsInRva2VuIjoiYzlmZGM1NGI4NjIxNmNmMyIsInBsIjoibXdlYiIsImRldmljZV9pZCI6ImRpa2Frb2tvIn0.9damNZPZMkmU3D-puzThqmDCSUeYNybMtmCdILqpl4M',
+    'Content-Type': ['application/json', 'text/plain']
+  },
+  body: "{\r\n  \"unique_code\": \"IN4DDR\",\r\n  \"event_id\": 8\r\n}"
+
+};
+request(options, function (error, response) { 
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
